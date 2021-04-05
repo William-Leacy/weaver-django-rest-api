9fffc628fa935872a8875e044b8d3d3e57361e54
William Leacy <will.leacy10@gmail.com>
2021-04-05 10:15:18 -0400

    Add connected django to postgres database

--- weaver/weaver_api/models.py
+++ weaver/weaver_api/models.py
@@ -5,3 +5,17 @@ class User(models.Model):
     username = models.CharField(max_length=60)
     def __str__(self):
         return self.username
+
+class Gallery(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE,primary_key=True,)
+    name = models.CharField(max_length=50, default='Gallery ')
+
+class WeavedImage(models.Model):
+    gallery = models.ForeignKey(Gallery, on_delete=models.CASCADE)
+    image_name = models.CharField(max_length=60)
+    image_source = models.ImageField(upload_to='user_images/', default='image')
+    image_url = models.URLField()
+    image_description = models.CharField(max_length=60)
+    image_creation_time = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return self.image_url
--- weaver/weaver_api/serializers.py
+++ weaver/weaver_api/serializers.py
@@ -1,9 +1,21 @@
 # serializers.py
 from rest_framework import serializers
 
-from .models import User
+from .models import User, Gallery, WeavedImage
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
         fields = ('id','username',)
+
+class GallerySerializer(serializers.HyperlinkedModelSerializer):
+    user_gallery = UserSerializer(many=True)
+    class Meta:
+        model = Gallery
+        fields = ('name','user_gallery')
+
+class WeavedImageSerializer(serializers.HyperlinkedModelSerializer):
+    gallery_image = GallerySerializer(many=True)
+    class Meta:
+        model = WeavedImage
+        fields = ('id','gallery','gallery_image','image_name','image_url','image_description','image_creation_time')
