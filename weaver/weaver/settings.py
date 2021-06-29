97958859b44e59a1b461be4ff70aa87fa435c984
William Leacy <will.leacy10@gmail.com>
2021-06-29 12:18:46 -0400

    Fix dockerfile update

--- weaver/weaver/settings.py
+++ weaver/weaver/settings.py
@@ -102,12 +102,12 @@ WSGI_APPLICATION = 'weaver.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': env("DATABASE_NAME"),
         'USER': env("DATABASE_USER"),
         'PASSWORD': env("DATABASE_PASSWORD"),
         'HOST': env("DATABASE_HOST"),
-        'PORT': '',
+        'PORT': '5432',
     }
 }
 
@@ -211,17 +211,17 @@ JWT_AUTH = {
 
 #S3 BUCKETS CONFIG
 
-AWS_ACCESS_KEY_ID = env('AWS_ACCESS_KEY_ID')
+AWS_ACCESS_KEY_ID = 'AKIA6H6OVFJUQ4J7RNMA'
 
-AWS_SECRET_ACCESS_KEY = env('AWS_SECRET_ACCESS_KEY')
+AWS_SECRET_ACCESS_KEY = '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'
 
-AWS_STORAGE_BUCKET_NAME = env('AWS_STORAGE_BUCKET_NAME')
+AWS_STORAGE_BUCKET_NAME = 'weaver-images-bucket'
 
 AWS_S3_FILE_OVERWRITE = False
 
 AWS_DEFAULT_ACL = None
 
-DEFAULT_FILE_STORAGE = env('DEFAULT_FILE_STORAGE')
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-STATICFILES_STORAGE = env('STATICFILES_STORAGE')
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
